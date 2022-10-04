1ce9f55c3944a7153477c9b6b5838360146f3a75
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-10-04 15:23:17 +0200

    ✨ adding form in html and post request to post an user, it works !

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ const characterCreateBtn = document.querySelector('.create_character')
 characterCreateBtn.addEventListener('click', () => {
     createCharacter();
 })
+    
 
 const character = fetch('http://localhost:5000/characters').then((data) => {
     return data.json();
@@ -39,6 +40,7 @@ const character = fetch('http://localhost:5000/characters').then((data) => {
         <div class="character_card">
             <div class="character_level">${values.level}</div>
             <div class="character_job">${values.job}</div>
+            <div class="character_life">${values.life}</div>
         </div>
         `
     });
@@ -46,6 +48,34 @@ const character = fetch('http://localhost:5000/characters').then((data) => {
 }).catch((err) => {
     console.log(err)
 })
+
+
+// User 
+
+const formEmail = document.querySelector('#email_name');
+const formPassword = document.querySelector('#password');
+const sendRegister = document.querySelector('#submit')
+console.log(formEmail, formPassword)
+
+const subscribe = async () => {
+    let register = {
+        email: formEmail.value,
+        password: formPassword.value
+    }
+
+    try {
+        const res = axios.post('http://localhost:5000/register', register);
+        console.log(res)
+    } catch(e) {
+        console(e)
+    }
+}
+
+sendRegister.addEventListener('click', () => {
+    subscribe();
+});
+
+
 
 
     
